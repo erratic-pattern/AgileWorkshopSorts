f132582b84786dbeed1fe07829e7d6f7456524db
Adam Curtis <kallisti.dev@gmail.com>
2016-11-01 10:23:12 -0600

    rename packages and classes

--- src/agileworkshopsorts/AgileWorkshopSorts.java
+++ src/agileworkshopsorts/AgileWorkshopSorts.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package agileworkshopsorts2;
+package agileworkshopsorts;
 
 /**
  *
  * @author Training
  */
-public class AgileWorkshopSorts2 {
+public class AgileWorkshopSorts {
 
     /**
      * @param args the command line arguments
